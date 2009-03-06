8aaea67a29205b8d3fe787d94d0dc5987858d2e6
westcott <westcott>
2009-03-06 15:02:30 +0000

    added full matrix files

---
+++ fullmatrix.cpp
@@ -0,0 +1,20 @@
+/*
+ *  fullmatrix.cpp
+ *  Mothur
+ *
+ *  Created by Sarah Westcott on 3/6/09.
+ *  Copyright 2009 Schloss Lab UMASS Amherst. All rights reserved.
+ *
+ */
+
+#include "fullmatrix.h"
+
+
+/**************************************************************************
+FullMatrix::FullMatrix(ifstream& in) {
+
+}
+~FullMatrix::FullMatrix(){}
+	
+	int FullMatrix::getNumSeqs(){}
+*/
