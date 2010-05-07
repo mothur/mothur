17aafaea968f87e581297063b16695ad515bea53
westcott <westcott>
2010-05-07 16:45:56 +0000

    modified trim.seqs to account for primers of different lengths

---
+++ makegroupcommand.h
@@ -0,0 +1,33 @@
+#ifndef MAKEGROUPCOMMAND_H
+#define MAKEGROUPCOMMAND_H
+
+/*
+ *  makegroupcommand.h
+ *  Mothur
+ *
+ *  Created by westcott on 5/7/10.
+ *  Copyright 2010 Schloss Lab. All rights reserved.
+ *
+ */
+
+#include "command.hpp"
+
+class MakeGroupCommand : public Command {
+	
+public:
+	MakeGroupCommand(string);	
+	~MakeGroupCommand();
+	int execute(); 
+	void help();	
+	
+private:
+		
+	string fastaFileName, groups, outputDir;
+	vector<string> fastaFileNames;
+	vector<string> groupsNames;
+	
+	bool abort;
+};
+
+#endif
+
