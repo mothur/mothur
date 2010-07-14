602c9308718e0726080977cb62e5844cd28d2cc3
pschloss <pschloss>
2010-07-14 12:14:53 +0000

    pat's changes to trim.seqs

---
+++ qualityscores.cpp
@@ -0,0 +1,295 @@
+/*
+ *  qualityscores.cpp
+ *  Mothur
+ *
+ *  Created by Pat Schloss on 7/12/10.
+ *  Copyright 2010 Schloss Lab. All rights reserved.
+ *
+ */
+
+#include "qualityscores.h"
+
+/**************************************************************************************************/
+
+QualityScores::QualityScores(){
+	try {
+		m = MothurOut::getInstance();
+		seqName = "";
+		seqLength = -1;
+	}
+	catch(exception& e) {
+		m->errorOut(e, "QualityScores", "QualityScores");
+		exit(1);
+	}							
+}
+
+/**************************************************************************************************/
+
+QualityScores::QualityScores(ifstream& qFile){
+	try {
+		
+		m = MothurOut::getInstance();
+
+		seqName = "";
+		seqLength = -1;
+		int score;
+		
+		string line;
+		getline(qFile, line);
+		istringstream nameStream(line);
+	
+		nameStream >> seqName;
+		seqName = seqName.substr(1); 
+
+		getline(qFile, line);
+		istringstream qualStream(line);
+	
+		while(qualStream){
+			qualStream >> score;
+			qScores.push_back(score);
+		}
+		qScores.pop_back();
+		
+		seqLength = qScores.size();	
+	}
+	catch(exception& e) {
+		m->errorOut(e, "QualityScores", "QualityScores");
+		exit(1);
+	}							
+
+}
+
+/**************************************************************************************************/
+
+string QualityScores::getName(){
+	
+	try {
+		return seqName;
+	}
+	catch(exception& e) {
+		m->errorOut(e, "QualityScores", "getName");
+		exit(1);
+	}							
+}
+
+/**************************************************************************************************/
+
+void QualityScores::printQScores(ofstream& qFile){
+	try {
+		
+		double aveQScore = calculateAverage();
+		
+		qFile << '>' << seqName << '\t' << aveQScore << endl;
+		
+		for(int i=0;i<seqLength;i++){
+			qFile << qScores[i] << ' ';
+		}
+		qFile << endl;
+				
+	}
+	catch(exception& e) {
+		m->errorOut(e, "QualityScores", "printQScores");
+		exit(1);
+	}							
+}
+
+/**************************************************************************************************/
+
+void QualityScores::trimQScores(int start, int end){
+	try {
+		vector<int> hold;
+		
+		if(end == -1){		
+			hold = vector<int>(qScores.begin()+start, qScores.end());
+			qScores = hold;		
+		}
+		if(start == -1){
+			hold = vector<int>(qScores.begin(), qScores.begin()+end);	//not sure if indexing is correct
+			qScores = hold;		
+		}
+
+		seqLength = qScores.size();
+	}
+	catch(exception& e) {
+		m->errorOut(e, "QualityScores", "trimQScores");
+		exit(1);
+	}							
+}
+
+/**************************************************************************************************/
+
+void QualityScores::flipQScores(){
+	try {
+		
+		vector<int> temp = qScores;
+		for(int i=0;i<seqLength;i++){
+			qScores[seqLength - i - 1] = temp[i];
+		}
+		
+	}
+	catch(exception& e) {
+		m->errorOut(e, "QualityScores", "flipQScores");
+		exit(1);
+	}							
+}
+
+/**************************************************************************************************/
+
+bool QualityScores::stripQualThreshold(Sequence& sequence, double qThreshold){
+	try {
+		string rawSequence = sequence.getUnaligned();
+		int seqLength = sequence.getNumBases();
+		
+		if(seqName != sequence.getName()){
+			m->mothurOut("sequence name mismatch btwn fasta: " + sequence.getName() + " and qual file: " + seqName);
+			m->mothurOutEndLine();	
+		}
+		
+		int end;
+		for(int i=0;i<seqLength;i++){
+			end = i;
+			if(qScores[i] < qThreshold){
+				break;
+			}
+		}
+		
+		sequence.setUnaligned(rawSequence.substr(0,end));
+		trimQScores(-1, end);
+	
+		return 1;
+	}
+	catch(exception& e) {
+		m->errorOut(e, "QualityScores", "flipQScores");
+		exit(1);
+	}							
+	
+}
+
+/**************************************************************************************************/
+
+bool QualityScores::stripQualRollingAverage(Sequence& sequence, double qThreshold){
+	try {
+		string rawSequence = sequence.getUnaligned();
+		int seqLength = sequence.getNumBases();
+		
+		if(seqName != sequence.getName()){
+			m->mothurOut("sequence name mismatch btwn fasta: " + sequence.getName() + " and qual file: " + seqName);
+			m->mothurOutEndLine();	
+		}
+		
+		int end = -1;
+		double rollingSum = 0.0000;
+		
+		for(int i=0;i<seqLength;i++){
+
+			rollingSum += (double)qScores[i];
+			
+			if(rollingSum / (double)(i+1) < qThreshold){
+				end = i;
+//				cout << i+1 << '\t' << seqName << '\t' << rollingSum / (double)(i+1) << endl;
+				break;
+			}
+		}
+		
+		if(end == -1){	end = seqLength;	}
+		
+		sequence.setUnaligned(rawSequence.substr(0,end));
+		trimQScores(-1, end);
+		
+		return 1;
+	}
+	catch(exception& e) {
+		m->errorOut(e, "QualityScores", "flipQScores");
+		exit(1);
+	}							
+	
+}
+
+/**************************************************************************************************/
+
+bool QualityScores::stripQualWindowAverage(Sequence& sequence, int stepSize, int windowSize, double qThreshold){
+	try {
+		string rawSequence = sequence.getUnaligned();
+		int seqLength = sequence.getNumBases();
+		
+		if(seqName != sequence.getName()){
+			m->mothurOut("sequence name mismatch btwn fasta: " + sequence.getName() + " and qual file: " + seqName);
+			m->mothurOutEndLine();	
+		}
+		
+		int end = windowSize;
+		int start = 0;
+		
+
+		while(start < seqLength){
+			double windowSum = 0.0000;
+
+			for(int i=start;i<end;i++){
+				windowSum += qScores[i];				
+			}
+			double windowAverage = windowSum / (double)(end-start);
+			
+			if(windowAverage < qThreshold){
+				end = start;
+				break;
+			}
+			start += stepSize;
+			end = start + windowSize;
+			if(end >= seqLength){	end = seqLength - 1;	}
+		}
+		
+		
+		if(end == -1){	end = seqLength;	}
+		
+		sequence.setUnaligned(rawSequence.substr(0,end));
+		trimQScores(-1, end);
+		
+		return 1;
+	}
+	catch(exception& e) {
+		m->errorOut(e, "QualityScores", "flipQScores");
+		exit(1);
+	}							
+	
+}
+
+/**************************************************************************************************/
+
+double QualityScores::calculateAverage(){
+	
+	double aveQScore = 0.0000;
+	
+	for(int i=0;i<seqLength;i++){
+		aveQScore += (double) qScores[i];
+	}
+	aveQScore /= (double) seqLength;
+	
+	return aveQScore;
+}
+
+/**************************************************************************************************/
+
+bool QualityScores::cullQualAverage(Sequence& sequence, double qAverage){
+	try {
+		string rawSequence = sequence.getUnaligned();
+		bool success = 0;	//guilty until proven innocent
+		
+		if(seqName != sequence.getName())	{
+			m->mothurOut("sequence name mismatch btwn fasta: " + sequence.getName() + " and qual file: " + seqName);
+			m->mothurOutEndLine();	
+		} 
+			
+		double aveQScore = calculateAverage();
+		
+		if(aveQScore >= qAverage)	{	success = 1;	}
+		else						{	success = 0;	}
+		
+		return success;
+	}
+	catch(exception& e) {
+		m->errorOut(e, "TrimSeqsCommand", "cullQualAverage");
+		exit(1);
+	}
+}
+
+/**************************************************************************************************/
---
+++ qualityscores.h
@@ -0,0 +1,44 @@
+#ifndef QUALITYSCORES
+#define QUALITYSCORES
+
+/*
+ *  qualityscores.h
+ *  Mothur
+ *
+ *  Created by Pat Schloss on 7/12/10.
+ *  Copyright 2010 Schloss Lab. All rights reserved.
+ *
+ */
+
+
+#include "mothur.h"
+#include "mothurout.h"
+#include "sequence.hpp"
+
+/**************************************************************************************************/
+
+class QualityScores {
+public:
+	QualityScores();
+	QualityScores(ifstream&);
+	string getName();
+	void printQScores(ofstream&);
+	void trimQScores(int, int);
+	void flipQScores();
+	bool stripQualThreshold(Sequence&, double);
+	bool stripQualRollingAverage(Sequence&, double);
+	bool stripQualWindowAverage(Sequence&, int, int, double);
+	bool cullQualAverage(Sequence&, double);
+private:
+	
+	double calculateAverage();
+	MothurOut* m;
+	vector<int> qScores;
+	
+	string seqName;
+	int seqLength;
+};
+	
+/**************************************************************************************************/
+
+#endif
