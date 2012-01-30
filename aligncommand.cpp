e8c014de33edac1fb91d7593e143bf1f5107ffce
Sarah Westcott <mothur.westcott@gmail.com>
2012-01-30 12:14:33 -0500

    test

--- aligncommand.cpp
+++ aligncommand.cpp
@@ -16,7 +16,7 @@
 
 #include "aligncommand.h"
 #include "referencedb.h"
-//test comment
+//test comm
 //**********************************************************************************************************************
 vector<string> AlignCommand::setParameters(){	
 	try {
