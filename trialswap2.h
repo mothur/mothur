c669e63b8cb1193d683a1016562d35b455f76575
Kathryn Iverson <kd.iverson@gmail.com>
2012-04-30 10:37:00 -0400

    re-wrote co-occurrence command

--- trialswap2.h
+++ trialswap2.h
@@ -23,21 +23,9 @@ public:
     double calc_pvalue_greaterthan (vector<double>, double);
     int swap_checkerboards (vector<vector<int> > &);
     int calc_combo (vector<vector<int> > &);
-    double calc_vratio (vector<int>, vector<int>);
-    int calc_checker (vector<vector<int> > &,vector<int>);
-    double calc_c_score (vector<vector<int> > &,vector<int>);
-    
-    int sim1 (vector<vector<int> > &);
-    void sim2(vector<vector<int> >&);
-    int sim2plus(vector<int>, vector<vector<int> > &);
-    void sim3(vector<vector<int> > &);
-    int sim4(vector<int>, vector<int>, vector<vector<int> > &);
-    int sim5(vector<int>, vector<int>, vector<vector<int> > &);
-    int sim6(vector<int>, vector<vector<int> > &);
-    int sim7(vector<int>, vector<vector<int> > &);
-    int sim8(vector<int>, vector<int>, vector<vector<int> > &);
-    int transpose_matrix (vector<vector<int> > &, vector<vector<int> > &);
-    int update_row_col_totals(vector<vector<int> > &, vector<int>&, vector<int>&);
+    double calc_vratio (int, int, vector<int>, vector<int>);
+    int calc_checker (vector<vector<int> > &, vector<int>, int, int);
+    double calc_c_score (vector<vector<int> > &, vector<int>, int, int);
 
     
 private:
