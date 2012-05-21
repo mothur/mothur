937bc27a26acc591cd8234c86dfde5d8e2dde109
Abu Zaher Md. Faridee <zaher14@gmail.com>
2012-05-21 06:12:10 +0600

    issue #14: created a new folder to implement the regulazied random forest feature selection algorithm. Once this implementation is complete, we would try to integrate the algorithm to mothur's codebase

---
+++ rrf-fs-prototype/rrf-fs-prototype.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  rrf-fs-prototype
+//
+//  Created by Abu Zaher Faridee on 5/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]){
+  cout << "Note: This is a very basic implementation of Regularized Random Forest Algorithm for feature selection. After the prototype has been done, we'd need to integrate this into mothur as a seperate module." << endl;
+  return 0;
+}
+
