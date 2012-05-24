82fd6705c31f7ab705314135ee3fd72b3644a72f
Abu Zaher Md. Faridee <zaher14@gmail.com>
2012-05-24 06:43:24 +0600

    issue #14: created file reader class, working on dataset generation code and others

--- rrf-fs-prototype/rrf-fs-prototype.cpp
+++ rrf-fs-prototype/rrf-fs-prototype.cpp
@@ -1,17 +1,47 @@
-//
-//  main.cpp
-//  rrf-fs-prototype
-//
-//  Created by Abu Zaher Faridee on 5/21/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
+  //
+  //  main.cpp
+  //  rrf-fs-prototype
+  //
+  //  Created by Abu Zaher Faridee on 5/21/12.
+  //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+  //
+  //  Note: This is a very basic implementation of Regularized Random Forest 
+  //  Algorithm for Feature Selection. After the prototype has been done, 
+  //  we'd need to integrate this into mothur as a seperate module.
+  //
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "sharedanddesignfilereader.h"
+
+class DataSet{
+public:
+private:
+  vector<string> featureLabels;
+  unsigned numOTUs;
+};
+
+class TrainingSet{
+  vector<int> otuCounts;
+  int outputClass;
+};
 
 using namespace std;
 
 int main(int argc, const char * argv[]){
-  cout << "Note: This is a very basic implementation of Regularized Random Forest Algorithm for feature selection. After the prototype has been done, we'd need to integrate this into mothur as a seperate module." << endl;
+  
+  string sharedFilePath = "final.an.0.03.subsample.0.03.pick.shared";
+  string designFilePath = "mouse.sex_time.design";
+  
+  SharedAndDesignFileReader sharedFileReader(sharedFilePath);
+  sharedFileReader.readFileContent();
+  
+  SharedAndDesignFileReader designFileReader(designFilePath);
+  designFileReader.readFileContent();
   return 0;
 }
 
---
+++ rrf-fs-prototype/sharedanddesignfilereader.h
@@ -0,0 +1,60 @@
+//
+//  sharedanddesignfilereader.h
+//  rrf-fs-prototype
+//
+//  Created by Abu Zaher Faridee on 5/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef rrf_fs_prototype_sharedanddesignfilereader_h
+#define rrf_fs_prototype_sharedanddesignfilereader_h
+
+using namespace std;
+
+class SharedAndDesignFileReader{
+  
+public:
+  SharedAndDesignFileReader(string filePath){
+    this->filePath = filePath;
+    
+  }
+  void readFileContent(){
+    inputFileStream.open(filePath.c_str());
+    while (!inputFileStream.eof()) {
+      getline(inputFileStream, lineBuffer);
+      stringStream << lineBuffer;
+      while (!stringStream.eof()) {
+        string temp;
+        getline(stringStream, temp, '\t');
+        lineStrings.push_back(temp);
+      }
+      stringStream.clear();
+      fileContent.push_back(lineStrings);
+      lineStrings.clear();
+    }
+    inputFileStream.close();
+  }
+  
+  void printFileContent(){
+    for (unsigned i = 0; i < fileContent.size(); ++i) {
+      vector<string> temp = fileContent[i];
+      for (unsigned j = 0; j < temp.size(); ++j) {
+        cout << temp[j] << "\t";
+      }
+      cout << endl << endl;
+    }
+  }
+  
+  vector< vector<string> > getFileContent(){ return fileContent; }
+  
+private:
+  string filePath;
+  ifstream inputFileStream;
+  stringstream stringStream;
+  string lineBuffer;
+  vector<string> lineStrings; 
+  vector< vector<string> > fileContent;
+};
+
+
+#endif
