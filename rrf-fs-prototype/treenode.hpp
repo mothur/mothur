23b70d574cc98eff77d718a832af79293c1b8433
Abu Zaher Md. Faridee <zaher14@gmail.com>
2012-07-22 15:18:45 +0600

    Issue #23: Implementing TreeNode class

--- rrf-fs-prototype/treenode.hpp
+++ rrf-fs-prototype/treenode.hpp
@@ -9,17 +9,72 @@
 #ifndef rrf_fs_prototype_treenode_hpp
 #define rrf_fs_prototype_treenode_hpp
 
-  // TreeNode is a passive class, we run operations on this, but it sould not
-  // contain any specific methods of "Regularized Random Forest" of itself
 class TreeNode{
 public:
-  TreeNode(vector<TrainingSet> samples) : 
-    samples(samples),
-    isLeaf(false){
+  TreeNode(vector< vector<int> > bootstrappedTrainingSamples,
+           vector<int> globalDiscardedFeatureIndices,
+           int numFeatures,
+           int numSamples,
+           int numOutputClasses,
+           int generation)
+  : bootstrappedTrainingSamples(bootstrappedTrainingSamples),
+  globalDiscardedFeatureIndices(globalDiscardedFeatureIndices),
+  numFeatures(numFeatures),
+  numSamples(numSamples),
+  numOutputClasses(numOutputClasses),
+  generation(generation),
+  isLeaf(false),
+  outputClass(-1),
+  splitFeatureIndex(0),
+  splitFeatureValue(0),
+  splitFeatureEntropy(0),
+  ownEntropy(0),
+  leftChildNode(NULL),
+  rightChildNode(NULL),
+  parentNode(NULL){
+//    self.bootstrappedFeatureVectors = [list(x) for x in zip(*self.bootstrappedTrainingSamples)]
+//    self.bootstrappedOutputVector = [bootstrappedTrainingSamples[x][self.numFeatures] for x in range(0, self.numSamples)]
+//    
+//# call some helper functions
+//    self.createLocalDiscardedFeatureList()
+//    
+//    self.updateNodeEntropy()
+
   }
   
-  vector<TrainingSet> samples;
+  ~TreeNode(){
+  }
+protected:
+private:
+  vector<vector<int> > bootstrappedTrainingSamples;
+  vector<int> globalDiscardedFeatureIndices;
+  int numFeatures;
+  int numSamples;
+  int numOutputClasses;
+  int generation;
   bool isLeaf;
+  int outputClass;
+  
+  int splitFeatureIndex;
+  int splitFeatureValue;
+  int splitFeatureEntropy;
+  int ownEntropy;
+  
+  vector<vector<int> > bootstrappedFeatureVectors;
+  vector<int> bootstrappedOutputVector;
+  vector<int> featureSubsetIndices;
+  
+  TreeNode* leftChildNode;
+  TreeNode* rightChildNode;
+  TreeNode* parentNode;
+  
+  vector<int> localDiscardedFeatureIndices;
+  
+  void createLocalDiscardedFeatureList(){
+  }
+  
+  void updateNodeEntropy(){
+  }
 };
 
 #endif
