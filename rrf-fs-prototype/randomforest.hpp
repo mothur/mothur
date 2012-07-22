5f07cd544732d5b842c2727b2c32f6dfaa5eabd7
Abu Zaher Md. Faridee <zaher14@gmail.com>
2012-07-22 15:20:23 +0600

    Issue #23: creating RandomForest class

---
+++ rrf-fs-prototype/randomforest.hpp
@@ -0,0 +1,14 @@
+//
+//  randomforest.hpp
+//  rrf-fs-prototype
+//
+//  Created by Abu Zaher Faridee on 7/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef rrf_fs_prototype_randomforest_hpp
+#define rrf_fs_prototype_randomforest_hpp
+
+
+
+#endif
