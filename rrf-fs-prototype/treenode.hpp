dcd4c89f2236e132be547798a28fd9dabec76904
Abu Zaher Md. Faridee <zaher14@gmail.com>
2012-07-24 22:03:45 +0600

    Issue #23: updating setter and getters in TreeNode class

--- rrf-fs-prototype/treenode.hpp
+++ rrf-fs-prototype/treenode.hpp
@@ -51,28 +51,35 @@ public:
     // getters
     // we need to return const reference so that we have the actual value and note a copy, 
     // plus we do not modify the value as well
-  const int& getSplitFeatureIndex(){ return splitFeatureIndex; }
+  const int& getSplitFeatureIndex() { return splitFeatureIndex; }
     // TODO: check if this works properly or returs a shallow copy of the data
-  const vector< vector<int> >& getBootstrappedTrainingSamples(){ return bootstrappedTrainingSamples; }
-  const int& getSplitFeatureValue(){ return splitFeatureValue; }
-  const int& getGeneration(){ return generation; }
-  const bool& checkIsLeaf(){ return isLeaf; }
+  const vector< vector<int> >& getBootstrappedTrainingSamples() { return bootstrappedTrainingSamples; }
+  const int& getSplitFeatureValue() { return splitFeatureValue; }
+  const int& getGeneration() { return generation; }
+  const bool& checkIsLeaf() { return isLeaf; }
     // TODO: fix this const pointer dillema
     // we do not want to modify the data pointer by getLeftChildNode
-  TreeNode* getLeftChildNode(){ return leftChildNode; }
-  TreeNode* getRightChildNode(){ return rightChildNode; }
-  const int& getOutputClass(){ return outputClass; }
-  const int& getNumSamples(){ return numSamples; }
-  const int& getNumFeatures(){ return numFeatures; }
-  const vector<int>& getLocalDiscardedFeatureIndices(){ return localDiscardedFeatureIndices; }
+  TreeNode* getLeftChildNode() { return leftChildNode; }
+  TreeNode* getRightChildNode() { return rightChildNode; }
+  const int& getOutputClass() { return outputClass; }
+  const int& getNumSamples() { return numSamples; }
+  const int& getNumFeatures() { return numFeatures; }
+  const vector<int>& getLocalDiscardedFeatureIndices() { return localDiscardedFeatureIndices; }
+  const vector< vector<int> >& getBootstrappedFeatureVectors() { return bootstrappedFeatureVectors; }
+  const vector<int>& getBootstrappedOutputVector() { return bootstrappedOutputVector; }
+  const vector<int>& getFeatureSubsetIndices() { return featureSubsetIndices; }
+  const double& getOwnEntropy() { return ownEntropy; }
   
     // setters
-  void setIsLeaf(bool isLeaf){ this->isLeaf = isLeaf; }
-  void setOutputClass(int outputClass){ this->outputClass = outputClass; }
-  void setFeatureSubsetIndices(vector<int> featureSubsetIndices){ this->featureSubsetIndices = featureSubsetIndices; }
-  void setLeftChildNode(TreeNode* leftChildNode){ this->leftChildNode = leftChildNode; }
-  void setRightChildNode(TreeNode* rightChildNode){ this->rightChildNode = rightChildNode; }
-  void setParentNode(TreeNode* parentNode){ this->parentNode = parentNode; }
+  void setIsLeaf(bool isLeaf) { this->isLeaf = isLeaf; }
+  void setOutputClass(int outputClass) { this->outputClass = outputClass; }
+  void setFeatureSubsetIndices(vector<int> featureSubsetIndices) { this->featureSubsetIndices = featureSubsetIndices; }
+  void setLeftChildNode(TreeNode* leftChildNode) { this->leftChildNode = leftChildNode; }
+  void setRightChildNode(TreeNode* rightChildNode) { this->rightChildNode = rightChildNode; }
+  void setParentNode(TreeNode* parentNode) { this->parentNode = parentNode; }
+  void setSplitFeatureIndex(int splitFeatureIndex) { this->splitFeatureIndex = splitFeatureIndex; }
+  void setSplitFeatureValue(int splitFeatureValue) { this->splitFeatureValue = splitFeatureValue; }
+  void setSplitFeatureEntropy(double splitFeatureEntropy) { this->splitFeatureEntropy = splitFeatureEntropy; }
   
 protected:
 private:
