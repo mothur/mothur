af9c7d97ba739428f88e453116cbfbdc06172dce
Sarah Westcott <mothur.westcott@gmail.com>
2015-02-05 15:38:06 -0500

    more progress on kmerAlign and qual scores

--- kmeralign.cpp
+++ kmeralign.cpp
@@ -50,7 +50,6 @@ void KmerAlign::align(string A, string B, vector<int> AQual, vector<int> BQual){
             cout << i << '\t' << kmer << endl;
             int j;
             for (j = 0; j < numKmers && kmerseen[kmer][j] != 0; j++) ;
-            printf("kmerseen: %i, %i, %i\n", numKmers, j, (kmer * numKmers) + j);
             if (j == numKmers) {
                 /* If we run out of storage, we lose k-mers. */
                 m->mothurOut("[WARNING]: Inssufficent number of kmer instances. Try increasing the kmer parameter.\n");
@@ -64,27 +63,25 @@ void KmerAlign::align(string A, string B, vector<int> AQual, vector<int> BQual){
         set<int> overlaps;
         for(int i=1;i<nKmersB;i++){
             kmer = kmerLibrary.getKmerNumber(B, i);
-            printf("kmerseen reverse: %i\n", kmer);
             for (int j = 0; j < numKmers && kmerseen[kmer][j] != 0; j++) { //for as many instances as we saw of this kmer, recoding overlap
                 int index = aLength + bLength - (bLength-i) - kmerseen[kmer][j] - 2;
                 if (index <= maxOverlap) { overlaps.insert(index); }
-                printf("kmerseen reverse: %i, %i, %i, %i\n", index, kmerseen[kmer][j], (bLength-i), i);
             }
         }
         
         //find best overlap
-        double bestProb = -1.0;
+        double bestProb = -1.38629 * (aLength+bLength);
         int bestOverlap = -1;
         for (set<int>::iterator it = overlaps.begin(); it != overlaps.end(); it++) {
-            int overlap = (*it) + 2; //2 = minoverlap
+            int index = *it;
+            int overlap = index + 2; //2 = minoverlap
             double probability = calcProb(A, B, overlap, AQual, BQual);
-            printf("overlap prob: %i, %i, %f\n", *it, overlap, probability);
             if (probability > bestProb && overlap >= 2) {
                 bestProb = probability;
                 bestOverlap = overlap;
             }
         }
-        cout << bestOverlap << '\t' << (aLength-(bestOverlap)) << endl;
+        
         if(bestOverlap != -1){
             if((aLength-bestOverlap) > 0){ //add gaps to the start of B
                 int numGaps = (aLength-bestOverlap);
@@ -116,28 +113,33 @@ double KmerAlign::calcProb(string A, string B, int overlap, vector<int> forwardQ
         double prob = 0;
         int aLength = A.length();
         int bLength = B.length();
-        double randomBase = log(0.25);
-        
+        int unknown, match, mismatch; unknown = 0; match = 0; mismatch = 0;
+    
         for (int i = 0; i < overlap; i++) {
             int findex = aLength + i - overlap;
-            int rindex = bLength - (bLength-i) - 1;
+            int rindex = i;
             if (findex < 0 || rindex < 0 || findex >= aLength || rindex >= bLength)
                 continue;
             char f = A[findex];
             char r = B[rindex];
             if ((f == 'N') || (r == 'N')) {
-                prob -= randomBase;
-            } else if ((f & r) != 0) {
-                //probability += qual_match_pear[PHREDCLAMP(forward[findex].qual)][PHREDCLAMP(forward[rindex].qual)];
-                double p = qual_score[PHREDCLAMP(forwardQual[findex])];
-                double q = qual_score[PHREDCLAMP(forwardQual[rindex])];
-                prob += (1.0 - (1.0 - q) * p / 3.0 - (1.0 - p) * q / 3.0 - 2.0 * (1.0 - p) * (1.0 - q) / 9.0);
+                unknown++;
+            } else if (r == f) {
+                match++;
             } else {
-                double p = qual_score[PHREDCLAMP(reverseQual[findex])];
-                double q = qual_score[PHREDCLAMP(reverseQual[rindex])];
-                prob += (1 - p) * q / 3 + (1 - q) * p / 3 + p * q / 2;
+                mismatch++;
             }
         }
+        //ln(0.25 * (1 - 2 * 0.36 + 0.36 * 0.36))
+        double pmatch = -2.278869;
+        //ln((3 * 0.36 - 2 * 0.36 * 0.36) / 18.0)
+        double pmismatch = -3.087848;
+        if (overlap >= aLength && overlap >= bLength) {
+            prob = (-1.38629 * unknown + match * pmatch + mismatch * mismatch);
+        } else {
+            prob = (-1.38629 * (aLength + bLength - 2 * overlap + unknown) + match * pmatch + mismatch * pmismatch);
+        }
+
         
         return prob;
     }
