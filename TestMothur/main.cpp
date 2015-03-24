a5e88d0b8520ef70d42881f6f346a7ab0ae958e4
Sarah Westcott <mothur.westcott@gmail.com>
2015-03-24 09:27:11 -0400

    Adds testing project to mothur
    Model test for sequence class begun
    
    Completes: Issue #13

---
+++ TestMothur/main.cpp
@@ -0,0 +1,12 @@
+//
+//  main.cpp
+//  TestMothur
+//
+//  Created by Sarah Westcott on 3/23/15.
+//  Copyright (c) 2015 Schloss Lab. All rights reserved.
+//
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+
+
