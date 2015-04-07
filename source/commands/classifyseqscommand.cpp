a089e1919ca107193d2c2272fdb637d9b3236db6
Sarah Westcott <mothur.westcott@gmail.com>
2015-04-07 12:03:25 -0400

    Mods to processor adjust classify.seqs

--- source/commands/classifyseqscommand.cpp
+++ source/commands/classifyseqscommand.cpp
@@ -995,12 +995,20 @@ int ClassifySeqsCommand::createProcesses(string taxFileName, string tempTaxFile,
             }else {
                 m->mothurOut("[ERROR]: unable to spawn the number of processes you requested, reducing number to " + toString(process) + "\n"); processors = process;
                 for (int i = 0; i < processIDS.size(); i++) { kill (processIDS[i], SIGINT); }
+                //wait to die
+                for (int i=0;i<processIDS.size();i++) {
+                    int temp = processIDS[i];
+                    wait(&temp);
+                }
+                m->control_pressed = false;
                 recalc = true;
                 break;
             }
 		}
         
         if (recalc) {
+            //test line, also set recalc to true.
+            //for (int i = 0; i < processIDS.size(); i++) { kill (processIDS[i], SIGINT); } for (int i=0;i<processIDS.size();i++) { int temp = processIDS[i]; wait(&temp); } m->control_pressed = false;  processors=3; m->mothurOut("[ERROR]: unable to spawn the number of processes you requested, reducing number to " + toString(processors) + "\n");
             for (int i = 0; i < lines.size(); i++) {  delete lines[i];  }  lines.clear();
             vector<unsigned long long> positions = m->divideFile(filename, processors);
             for (int i = 0; i < (positions.size()-1); i++) {	lines.push_back(new linePair(positions[i], positions[(i+1)]));	}
