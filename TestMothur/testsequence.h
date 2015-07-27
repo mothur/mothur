92677405aea8482d611da5f81245924cb608d2a2
Sarah Westcott <mothur.westcott@gmail.com>
2015-07-27 14:17:44 -0400

    Experiments unit testing private class functions
    Issue #124

---
+++ TestMothur/testsequence.h
@@ -0,0 +1,23 @@
+//
+//  testsequence.h
+//  Mothur
+//
+//  Created by Sarah Westcott on 7/27/15.
+//  Copyright (c) 2015 Schloss Lab. All rights reserved.
+//
+
+#ifndef Mothur_testsequence_h
+#define Mothur_testsequence_h
+#include "sequence.hpp"
+
+class TestSequence : public Sequence {
+    
+    public:
+    
+    //using Sequence::Sequence;
+    using Sequence::initialize;
+    
+};
+
+
+#endif
