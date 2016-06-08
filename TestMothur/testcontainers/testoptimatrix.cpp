d80a71aca82e64149356dcaefdd2fc82cecd124f
Sarah Westcott <mothur.westcott@gmail.com>
2016-06-08 11:36:33 -0400

    Test datasets for optimatrix

---
+++ TestMothur/testcontainers/testoptimatrix.cpp
@@ -0,0 +1,9 @@
+//
+//  testoptimatrix.cpp
+//  Mothur
+//
+//  Created by Sarah Westcott on 6/6/16.
+//  Copyright (c) 2016 Schloss Lab. All rights reserved.
+//
+
+#include "testoptimatrix.h"
---
+++ TestMothur/testcontainers/testoptimatrix.h
@@ -0,0 +1,28 @@
+//
+//  testoptimatrix.h
+//  Mothur
+//
+//  Created by Sarah Westcott on 6/6/16.
+//  Copyright (c) 2016 Schloss Lab. All rights reserved.
+//
+
+#ifndef __Mothur__testoptimatrix__
+#define __Mothur__testoptimatrix__
+
+#include "optimatrix.h"
+
+class TestOptiMatrix : public OptiMatrix {
+    
+public:
+    
+    TestOptiMatrix();
+    ~TestOptiMatrix();
+    
+    using OptiMatrix::getCloseSeqs;
+    using OptiMatrix::findDistFormat;
+    using OptiMatrix::readPhylip;
+    using OptiMatrix::readColumn;
+    
+};
+
+#endif /* defined(__Mothur__testoptimatrix__) */
