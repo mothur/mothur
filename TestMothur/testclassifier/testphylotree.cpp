5e7ff3207f09791d8be87b2d1661863591cc1299
Sarah Westcott <mothur.westcott@gmail.com>
2016-08-30 11:24:34 -0400

    WIP adding tests
    #265

---
+++ TestMothur/testclassifier/testphylotree.cpp
@@ -0,0 +1,9 @@
+//
+//  testphylotree.cpp
+//  Mothur
+//
+//  Created by Sarah Westcott on 8/29/16.
+//  Copyright © 2016 Schloss Lab. All rights reserved.
+//
+
+#include "testphylotree.hpp"
---
+++ TestMothur/testclassifier/testphylotree.hpp
@@ -0,0 +1,28 @@
+//
+//  testphylotree.hpp
+//  Mothur
+//
+//  Created by Sarah Westcott on 8/29/16.
+//  Copyright © 2016 Schloss Lab. All rights reserved.
+//
+
+#ifndef testphylotree_hpp
+#define testphylotree_hpp
+
+class TestPhyloTree : public PhyloTree {
+    
+    
+public:
+    
+    TestPhyloTree();
+    ~TestPhyloTree();
+    
+    MothurOut* m;
+    
+    //using PhyloTree::
+    //using PhyloTree::  
+    
+};
+
+
+#endif /* testphylotree_hpp */
