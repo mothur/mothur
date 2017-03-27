6fa006c3486115e453678e01f9eef47328aef914
Sarah Westcott <mothur.westcott@gmail.com>
2017-03-27 10:40:15 -0400

    Adds Boost line to log file

--- source/mothur.cpp
+++ source/mothur.cpp
@@ -117,6 +117,11 @@ int main(int argc, char *argv[]){
 				m->mothurOutJustToLog("Using ReadLine");
 				m->mothurOutEndLine(); m->mothurOutEndLine();
 			#endif
+            
+            #ifdef USE_BOOST
+                m->mothurOutJustToLog("Using Boost");
+                m->mothurOutEndLine(); m->mothurOutEndLine();
+            #endif
 			
 			#ifdef MOTHUR_FILES
 				m->mothurOutJustToLog("Using default file location " + temp);
