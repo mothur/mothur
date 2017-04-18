a51e42cf1d06543336f3f5c96f3480604f091b3a
Sarah Westcott <mothur.westcott@gmail.com>
2017-04-18 15:46:45 -0400

    Fakes tp,tn,fp,fn
    #196

---
+++ TestMothur/fakes/fakemcc.hpp
@@ -0,0 +1,33 @@
+//
+//  fakemcc.hpp
+//  Mothur
+//
+//  Created by Sarah Westcott on 4/18/17.
+//  Copyright © 2017 Schloss Lab. All rights reserved.
+//
+
+#ifndef fakemcc_hpp
+#define fakemcc_hpp
+
+#include "mothurout.h"
+
+
+class FakeClusterCalc {
+    
+public:
+    
+    FakeClusterCalc() { m = MothurOut::getInstance(); tp = 823; tn = 1944106; fp = 95; fn = 354; }
+    ~FakeClusterCalc() {}
+    
+    
+    long long tp, tn, fp, fn;
+    
+protected:
+    MothurOut* m;
+    
+};
+
+
+
+#endif /* fakemcc_hpp */
+
