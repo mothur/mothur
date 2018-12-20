1e3deb87ade14dec6cad2cffea9dd8c575edee71
Sarah Westcott <mothur.westcott@gmail.com>
2018-12-20 13:01:21 -0500

    Removes notifyDisplays, makes part of update functions
    #544 #240

--- source/collectorscurvedata.h
+++ source/collectorscurvedata.h
@@ -14,17 +14,10 @@ class CollectorsCurveData : public Observable {
 public:
 	CollectorsCurveData() : rank(0) {};
 	
-	void registerDisplay(Display* o)		{	displays.insert(o);				};
-	//void removeDisplay(Display* o)			{	displays.erase(o);	delete o;	};
-	SAbundVector* getRankData()				{	return rank;					};
-	void rankDataChanged()					{	notifyDisplays();				};
-	void updateRankData(SAbundVector* rv)	{	rank = rv; rankDataChanged();	};
-
-	void notifyDisplays(){	
-		for(set<Display*>::iterator pos=displays.begin();pos!=displays.end();pos++){
-			(*pos)->update(rank);
-		}	
-	};
+	void registerDisplay(Display* o)		{	displays.insert(o);				}
+	SAbundVector* getRankData()				{	return rank;					}
+	void rankDataChanged()					{  for(set<Display*>::iterator pos=displays.begin();pos!=displays.end();pos++){ (*pos)->update(rank); } }
+	void updateRankData(SAbundVector* rv)	{	rank = rv; rankDataChanged();	}
 	
 private:
 	set<Display*> displays;
@@ -38,31 +31,31 @@ private:
 class SharedCollectorsCurveData : public Observable {
 	
 public:
-	SharedCollectorsCurveData() { }; //: shared1(0), shared2(0)
+	SharedCollectorsCurveData() {}
 	
-	void registerDisplay(Display* o)		{	displays.insert(o);			};
-	//void removeDisplay(Display* o)			{	displays.erase(o);	delete o;	};
-	void SharedDataChanged()				{	notifyDisplays();				};
-    void updateSharedData(vector<SharedRAbundVector*> s, int numSeqs, int numGroupComb, bool p, vector<string> g)	{	pairs = p; shared = s; NumSeqs = numSeqs; NumGroupComb = numGroupComb; groups = g; SharedDataChanged();	};
-	
-	void notifyDisplays(){	
-		for(set<Display*>::iterator pos=displays.begin();pos!=displays.end();pos++){
-			
-			if ((*pos)->calcNeedsAll() ) {
-				(*pos)->update(shared, NumSeqs, NumGroupComb, groups);
-			}else{
-				
-				if ( ((*pos)->isCalcMultiple() ) && ((*pos)->getAll() ) && (!pairs) ) {
-					(*pos)->update(shared, NumSeqs, NumGroupComb, groups);
-				}else {
-					vector<SharedRAbundVector*> temp; temp.push_back(shared[0]); temp.push_back(shared[1]);
-					shared = temp; 
-					
-					(*pos)->update(shared, NumSeqs, NumGroupComb, groups);
-				}
-			}
-		}	
-	};
+	void registerDisplay(Display* o)		{	displays.insert(o);			}
+    
+	void SharedDataChanged()				{
+        for(set<Display*>::iterator pos=displays.begin();pos!=displays.end();pos++){
+            
+            if ((*pos)->calcNeedsAll() ) {  (*pos)->update(shared, NumSeqs, NumGroupComb, groups);  }
+            else{
+                
+                if ( ((*pos)->isCalcMultiple() ) && ((*pos)->getAll() ) && (!pairs) ) { (*pos)->update(shared, NumSeqs, NumGroupComb, groups); }
+                else {
+                    vector<SharedRAbundVector*> temp; temp.push_back(shared[0]); temp.push_back(shared[1]);
+                    shared = temp; 
+                    
+                    (*pos)->update(shared, NumSeqs, NumGroupComb, groups);
+                }
+            }
+        }
+    }
+    
+    void updateSharedData(vector<SharedRAbundVector*> s, int numSeqs, int numGroupComb, bool p, vector<string> g)	{
+        pairs = p; shared = s; NumSeqs = numSeqs; NumGroupComb = numGroupComb; groups = g;
+        SharedDataChanged();
+    }
 	
 private:
 	set<Display*> displays;
