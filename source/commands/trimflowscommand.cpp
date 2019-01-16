10b22c4943ea1dc822899116d267be07bea2afb7
Sarah Westcott <mothur.westcott@gmail.com>
2019-01-16 14:21:50 -0500

    Fixes crash with trim.flows if all sequences are scrapped

--- source/commands/trimflowscommand.cpp
+++ source/commands/trimflowscommand.cpp
@@ -290,7 +290,7 @@ int TrimFlowsCommand::execute(){
         outputTypes["file"].push_back(flowFilesFileName);
         outputNames.push_back(flowFilesFileName);
         
-		if(allFiles){
+		if((allFiles) && (groupMap.size() != 0)) {
             //print group file
             string groupFileName = getOutputFileName("group",variables);
             ofstream out; util.openOutputFile(groupFileName, out);
@@ -847,7 +847,7 @@ int TrimFlowsCommand::createProcessesCreateTrim(string flowFileName, string trim
         if (fasta) { delete threadFastaWriter; }
         delete dataBundle;
         
-        m->mothurOut("It took " + toString(time(NULL) - start) + " secs to trim " + toString(num) + " sequences."); if (m->getDebug()) {   m->mothurOut("Scrapped " + toString(badNames.size()) + ".");  } m->mothurOutEndLine();
+        m->mothurOut("It took " + toString(time(NULL) - start) + " secs to trim " + toString(num) + " sequences."); if (m->getDebug()) {   m->mothurOut("Scrapped " + toString(badNames.size()) + ".\n");  } 
         
 		return num;
 	}
