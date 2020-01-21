b28820129e86c595ba1031e89c005106ee385830
Sarah Westcott <mothur.westcott@gmail.com>
2020-01-21 14:03:30 -0500

    Adds getNextShared to get.relabund
    #240 #657

--- source/commands/getrelabundcommand.cpp
+++ source/commands/getrelabundcommand.cpp
@@ -178,76 +178,27 @@ int GetRelAbundCommand::execute(){
 		out.setf(ios::fixed, ios::floatfield); out.setf(ios::showpoint);
 		
         InputData input(sharedfile, "sharedfile", Groups);
-		lookup = input.getSharedRAbundVectors();
+        set<string> processedLabels;
+        set<string> userLabels = labels;
+        string lastLabel = "";
+        
+        SharedRAbundVectors* lookup = util.getNextShared(input, allLines, userLabels, processedLabels, lastLabel);
         Groups = lookup->getNamesGroups();
-		string lastLabel = lookup->getLabel();
+
         vector<string> binLabels = lookup->getOTUNames();
         out << "label\tGroup\tnumOtus";
         for (int i = 0; i < binLabels.size(); i++) { out  << '\t' << binLabels[i]; } out << endl;
-		
-		//if the users enters label "0.06" and there is no "0.06" in their file use the next lowest label.
-		set<string> processedLabels;
-		set<string> userLabels = labels;
-
-		//as long as you are not at the end of the file or done wih the lines you want
-		while((lookup != NULL) && ((allLines == 1) || (userLabels.size() != 0))) {
+        
+        while (lookup != NULL) {
+            
+            if (m->getControl_pressed()) { delete lookup; break; }
+            
+            getRelAbundance(lookup, out); delete lookup;
+            
+            lookup = util.getNextShared(input, allLines, userLabels, processedLabels, lastLabel);
+        }
+        out.close();
 			
-            if (m->getControl_pressed()) {  outputTypes.clear();  delete lookup;   out.close(); util.mothurRemove(outputFileName); return 0; }
-	
-			if(allLines == 1 || labels.count(lookup->getLabel()) == 1){
-
-				m->mothurOut(lookup->getLabel()+"\n"); 
-				getRelAbundance(lookup, out);
-				
-				processedLabels.insert(lookup->getLabel()); userLabels.erase(lookup->getLabel());
-			}
-			
-			if ((util.anyLabelsToProcess(lookup->getLabel(), userLabels, "") ) && (processedLabels.count(lastLabel) != 1)) {
-				string saveLabel = lookup->getLabel();
-			
-				delete lookup;
-				lookup = input.getSharedRAbundVectors(lastLabel);
-				m->mothurOut(lookup->getLabel()+"\n"); 
-				getRelAbundance(lookup, out);
-				
-				processedLabels.insert(lookup->getLabel()); userLabels.erase(lookup->getLabel());
-				
-				//restore real lastlabel to save below
-				lookup->setLabels(saveLabel);
-			}
-			
-			lastLabel = lookup->getLabel();
-			//prevent memory leak
-			delete lookup;
-			
-			if (m->getControl_pressed()) {  outputTypes.clear();   out.close(); util.mothurRemove(outputFileName); return 0; }
-
-			//get next line to process
-			lookup = input.getSharedRAbundVectors();
-		}
-		
-		if (m->getControl_pressed()) { outputTypes.clear();   out.close(); util.mothurRemove(outputFileName);  return 0; }
-
-		//output error messages about any remaining user labels
-		bool needToRun = false;
-		for (set<string>::iterator it = userLabels.begin(); it != userLabels.end(); it++) {
-			m->mothurOut("Your file does not include the label " + *it); 
-            if (processedLabels.count(lastLabel) != 1)  { m->mothurOut(". I will use " + lastLabel + ".\n"); needToRun = true;  }
-			else                                        { m->mothurOut(". Please refer to " + lastLabel + ".\n");               }
-		}
-	
-		//run last label if you need to
-		if (needToRun )  {
-            delete lookup;
-			lookup = input.getSharedRAbundVectors(lastLabel);
-			
-			m->mothurOut(lookup->getLabel()+"\n"); 
-			getRelAbundance(lookup, out);
-			
-			delete lookup;
-		}
-		out.close();
-		
 		if (m->getControl_pressed()) { outputTypes.clear(); util.mothurRemove(outputFileName); return 0;    }
 		
 		m->mothurOut("\nOutput File Names: \n"); 
